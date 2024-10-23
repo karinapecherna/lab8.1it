830e5566a3efcc4d77f918d99ab9f6fcba7321c0
Карина Печерная <karina.pecherna.03@gmail.com>
2024-10-23 14:57:17 +0300

    l

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab8.1/FileName.cpp"  
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestChangeFunction)
+        {
+            char input[101];
+            strcpy_s(input, sizeof(input), "Hello OGO World!");
+
+            
+            char* result = Change(input);
+
+            
+            Assert::AreEqual("Hello ** World!", result);
+        }
+    };
+}
---
+++ lab8.1/FileName.cpp
@@ -0,0 +1,76 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <cstring> 
+using namespace std;
+
+
+int Count(char* str_s) {
+    if (strlen(str_s) < 3) {
+        return 0; 
+    }
+    int k = 0;
+    for (int i = 0; str_s[i + 2] != 0; i++) {
+        if ((str_s[i] == 'O' && str_s[i + 1] == 'G' && str_s[i + 2] == 'O') ||
+            (str_s[i] == 'A' && str_s[i + 1] == 'G' && str_s[i + 2] == 'A')) {
+            k++; 
+        }
+    }
+    return k;
+}
+
+
+char* Change(char* str_s) {
+    size_t len = strlen(str_s);
+    if (len < 3) {
+        return str_s;
+    }
+    
+    char* tmp = new char[len * 2 + 1]; 
+    char* t = tmp; 
+    tmp[0] = '\0'; 
+    size_t i = 0;
+
+   
+    while (i < len && str_s[i + 2] != 0) {
+        if ((str_s[i] == 'O' && str_s[i + 1] == 'G' && str_s[i + 2] == 'O') ||
+            (str_s[i] == 'A' && str_s[i + 1] == 'G' && str_s[i + 2] == 'A')) {
+            strcat(t, "**"); 
+            t += 2; 
+            i += 3; 
+        }
+        else {
+            *t++ = str_s[i++];
+            *t = '\0'; 
+        }
+    }
+
+    
+    while (i < len) {
+        *t++ = str_s[i++];
+    }
+    *t = '\0'; 
+
+    strcpy(str_s, tmp);
+    return tmp; 
+}
+
+int main() {
+    char str_s[101];
+    cout << "Enter string:" << endl;
+    cin.getline(str_s, 100);
+
+    
+    cout << "String contained " << Count(str_s) << " groups of 'OGO' or 'AGA'" << endl;
+
+   
+    char* dest = new char[151];
+    dest = Change(str_s);
+
+    
+    cout << "Modified string (param) : " << str_s << endl;
+    cout << "Modified string (result): " << dest << endl;
+
+    
+    delete[] dest;
+    return 0;
+}
